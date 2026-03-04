39ceebccb2033bb2d6836ee2887d2dfdb9539cf9
kiakchi <kiakchi36@gmail.com>
2026-03-04 16:39:17 +0900

    2026.03.04

---
+++ QuizBee.py
@@ -0,0 +1,21 @@
+# This is a quiz bee game
+
+def quizbee_questions():
+    pass
+
+def check_answer():
+    pass
+
+def display_score():
+    pass
+
+def play_again():
+    pass
+
+easy_questions = {
+    "What Renaissance artist is buried in Rome's Pantheon":"B",
+
+}
+
+easy_answer = ["A: ", "B: Rafael", "C: ", "D: "]
+

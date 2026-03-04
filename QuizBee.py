3ef6cd429c8cef6514e2cc15b36e9118a97f468d
kiakchi <kiakchi36@gmail.com>
2026-03-04 17:28:36 +0900

    2026.03.04

--- QuizBee.py
+++ QuizBee.py
@@ -1,4 +1,4 @@
-# This is a quiz bee game
+# This is a quiz bee game for kids
 
 def quizbee_questions():
     pass
@@ -13,9 +13,44 @@ def play_again():
     pass
 
 easy_questions = {
-    "What Renaissance artist is buried in Rome's Pantheon":"B",
-
+    "What color do you get when you mix blue and yellow?":"B",
+    "How many legs does a spider have?":"A",
+    "What is the tallest land animal?":"B",
+    "What do caterpillars turn into?":"C",
+    "What do we call baby dogs?":"D"
 }
 
-easy_answer = ["A: ", "B: Rafael", "C: ", "D: "]
+med_questions = {
+    "What do plants need from sunlight to make food?":"D",
+    "What gas do we breathe in that our bodies need?":"D",
+    "What is the largest ocean on Earth?":"B",
+    "Which animal is famous for black-and-white stripes?":"A",
+    "Which planet is known as the Red Planet?":"C"
+}
+hard_questions = {
+    "What do we call Earth’s blanket of air?":"C",
+    "Which continent has the most people?":"A",
+    "What force keeps us on the ground?":"A",
+    "What is the center of an atom called?":"B",
+    "What is the freezing point of water in Celsius?":"D"
+
+
+}
+easy_answer = [["A: ", "B: Green", "C: ", "D: "],
+              ["A: Eight", "B: ", "C:  ", "D: "],
+              ["A: " , "B: Giraffe", "C: ", "D: "],
+              ["A: ", "B: ", "C: Butterflies", "D: "],
+              ["A: ", "B: ", "C: ", "D: Puppies "]]
+med_answer = [["A: ", "B: ", "C: ", "D: Energy for photosynthesis"],
+              ["A: ", "B: ", "C: ", "D: Oxygen "],
+              ["A: ", "B: Pacific Ocean", "C: ", "D: "],
+              ["A: Zebra", "B: ", "C: ", "D: "],
+              ["A: ", "B: ", "C: Mars", "D: "]]
+hard_answer = [["A: ", "B: ", "C: The atmosphere", "D: "],
+               ["A: Asia", "B: ", "C: ", "D: "],
+               ["A: Gravity", "B: ", "C: ", "D: "],
+               ["A: ", "B: The nucleus", "C: ", "D: "],
+               ["A: ", "B: ", "C: ", "D: 0°C"]]
+
+
 

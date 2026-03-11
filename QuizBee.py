76fd5bfc2ad4c69b177221c86c56b669287a0325
kiakchi <kiakchi36@gmail.com>
2026-03-11 18:23:00 +0900

    2026.03.05

--- QuizBee.py
+++ QuizBee.py
@@ -64,6 +64,7 @@ def medround():
 
     mdisplay_score(medcorrect_answer, med_ans)
     return medcorrect_answer
+
 def medcheck_answer(mcorrect_answer, med_ans):
     if mcorrect_answer == med_ans:
         print("Correct!")
@@ -71,7 +72,7 @@ def medcheck_answer(mcorrect_answer, med_ans):
         return 1
 
     else:
-        for a in easy_questions:
+        for a in med_questions:
             print("Incorrect!")
             print("**************************")
             return 0
@@ -85,8 +86,54 @@ def mdisplay_score(medcorrect_answer, med_ans):
     input("Press enter to continue...")
     print("++++++++++++++++++++++++++")
 
+def hardround():
+    answer = []
+    hardcorrect_answer = 0
+    hardopt_num = 1
+    print("**************************")
+    print("HARD Categories")
+    print("**************************")
+    for hround in hard_questions:
+        print(hround)
+        for hardopt in hard_option[hardopt_num - 1]:
+            print(hardopt)
+        hard_ans = input("Please enter your answer (A, B, C, or D): ")
+        hard_ans = hard_ans.upper()
+        answer.append(hard_ans)
+        hardcorrect_answer += hardcheck_answer(hard_questions.get(hround),
+                                             hard_ans)  # (med_questions.get(mround) - means to get the value from dictionary
+        hardopt_num += 1
+
+    hardisplay_score(hardcorrect_answer, hard_ans)
+    return hardcorrect_answer
+
+def hardcheck_answer(hcorrect_answer, hard_ans):
+    if hcorrect_answer == hard_ans:
+        print("Correct!")
+        print("**************************")
+        return 1
+
+    else:
+        for a in hard_questions:
+            print("Incorrect!")
+            print("**************************")
+            return 0
+def hardisplay_score(hardcorrect_answer, hard_ans):
+    print("*******************************")
+    print("Results:")
+    print("You got", hardcorrect_answer, "correct answers for Hard round")
+    print("Round 3 finished")
+    print("*******************************")
+    input("Press enter to continue...")
+    print("++++++++++++++++++++++++++")
 def play_again():
-    pass
+    playagain = input("Do you want to play again: (yes or no): ")
+    playagain = playagain.upper()
+
+    if playagain == "YES" or playagain == "Y":
+        return True
+    else:
+        return False
 
 easy_questions = {
     "What color do you get when you mix blue and yellow?":"B",
@@ -132,7 +179,7 @@ hard_option = [["A: ", "B: ", "C: The atmosphere", "D: "],
 #main program
 name = input("Enter player name: ")
 print("*************************")
-print("Welcome" + " " + name)
+print("Welcome:" + " " + name)
 print("*************************")
 print("This is a Quiz Game for kids")
 print("There will be 3 Categories")
@@ -143,10 +190,36 @@ input("Press enter to continue...")
 
 round_1 = easyround()
 round_2 = medround()
-round_3 = hardround()
-
-
 
 print("Lets recap the score")
 print("Round 1 score: ", round_1)
 print("Round 2 score: ", round_2)
+input("press enter to continue")
+
+round_3 = hardround()
+print("*************************")
+print("Lets recap the score")
+print("*************************")
+print("Round 1 score: ", round_1)
+print("Round 2 score: ", round_2)
+print("Round 3 score: ", round_3)
+print("*************************")
+
+while play_again():
+    easyround()
+    medround()
+    print("Lets recap the score")
+    print("Round 1 score: ", round_1)
+    print("Round 2 score: ", round_2)
+    input("press enter to continue")
+    hardround()
+    print("*************************")
+    print("Lets recap the score")
+    print("*************************")
+    print("Round 1 score: ", round_1)
+    print("Round 2 score: ", round_2)
+    print("Round 3 score: ", round_3)
+    print("*************************")
+
+print("See you")
+
